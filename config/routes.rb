84d4561ade2b26794e9dbb20a14493e8b36afc37
amelie <amelieoller@gmail.com>
2017-12-13 18:59:58 -0800

    Add routes and api namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :crops, except: [:new, :edit]
+    resources :varietals, except: [:new, :edit]
+    resources :plantings, except: [:new, :edit]
+  end
 end
