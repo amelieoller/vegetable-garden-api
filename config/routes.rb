961395ca4861e32adba059fbac25da1fa52502ff
amelie <amelieoller@gmail.com>
2017-12-20 15:29:59 -0800

    Remove obsolete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :crops, except: [:new, :edit]
-    resources :varietals, except: [:new, :edit]
-    resources :plantings, except: [:new, :edit]
   end
 end
